7b61046077903522c9fd34f2a70f0e7d0259f7f8
JuanCarlosLuna <jcluna.tech@gmail.com>
2026-03-20 21:32:20 +0100

    feat: add AuthService, AuthServiceImpl, DTOs, JwtService, SecurityConfig and exceptions

---
+++ backend/auth-api/src/main/java/com/jcluna/auth_api/security/JwtService.java
@@ -0,0 +1,62 @@
+package com.jcluna.auth_api.security;
+
+
+import com.jcluna.auth_api.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public boolean validateToken(String token, String email) {
+        String tokenEmail = Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+
+        return tokenEmail.equals(email) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+
+        return expiration.before(new Date());
+    }
+
+}
