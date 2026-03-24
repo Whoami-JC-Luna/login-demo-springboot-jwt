2bac57456af1c0fc52ce8499009e589af26a03aa
JuanCarlosLuna <jcluna.tech@gmail.com>
2026-03-24 15:23:02 +0100

    fix: update UserRepository primary key type and remove unused UserDetails overrides

--- backend/auth-api/src/main/java/com/jcluna/auth_api/repository/UserRepository.java
+++ backend/auth-api/src/main/java/com/jcluna/auth_api/repository/UserRepository.java
@@ -5,9 +5,10 @@ import com.jcluna.auth_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
 }
